fb10d1d41f080d79e87aa7ee4f80ceb60100f7d1
Nathan Mbugua <nathanngethe@gmail.com>
2023-07-15 21:24:38 +0300

    finalise ride adding

--- ride_share/lib/src/features/home/screens/requests/ride_request.dart
+++ ride_share/lib/src/features/home/screens/requests/ride_request.dart
@@ -1,7 +1,13 @@
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ride_share/src/constants/colors.dart';
 import 'package:ride_share/src/constants/image_strings.dart';
+import 'package:ride_share/src/features/home/screens/requests/live_location.dart';
+import 'package:ride_share/src/models/ride_request_model.dart';
+import 'package:ride_share/src/models/user_model.dart';
+import 'package:ride_share/src/repository/ride_repository.dart';
+import 'package:ride_share/src/repository/user_repository.dart';
 
 class RideRequests extends StatefulWidget {
   @override
@@ -9,7 +15,10 @@ class RideRequests extends StatefulWidget {
 }
 
 class _RideRequestsState extends State<RideRequests> {
+  RideRepository rideRepository = Get.put(RideRepository.instance);
   final double _borderRadius = 24;
+  // UserModel user =
+  //     UserRepository.instance.getUserDetails("+254706446072") as UserModel;
 
   var items = [
     PlaceInfo('Mary Jane', 4.4, 'Chalbi Court', 350),
@@ -26,55 +35,73 @@ class _RideRequestsState extends State<RideRequests> {
         backgroundColor: tSecondaryColor,
       ),
       body: ListView.builder(
-        itemCount: items.length,
+        shrinkWrap: true,
+        itemCount: 4,
         itemBuilder: (context, index) {
-          return Center(
-              child: Container(
-                width: 300,
-                height: 200,
-                padding: new EdgeInsets.all(10.0),
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  color: Colors.red,
-                  elevation: 10,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      const ListTile(
-                        leading: Icon(Icons.album, size: 60),
-                        title: Text(
-                            'Sonu Nigam',
-                            style: TextStyle(fontSize: 30.0)
-                        ),
-                        subtitle: Text(
-                            'Best of Sonu Nigam Music.',
-                            style: TextStyle(fontSize: 18.0)
-                        ),
-                      ),
-                      ButtonBar(
-                        children: <Widget>[
-                          ElevatedButton(
-                            child: const Text('Play'),
-                            onPressed: () {/* ... */},
-                          ),
-                          ElevatedButton(
-                            child: const Text('Pause'),
-                            onPressed: () {/* ... */},
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+          return Column(
+            children: [
+              ListTile(
+                tileColor: Colors.white,
+                leading: const Icon(Icons.person),
+                title: Text(items[index].name),
+                trailing: ElevatedButton(onPressed: () {
+                  Get.to(() => const LiveTrackingPage());
+                }, child: Text("Pickup"),),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(items[index].location),
+                    Text("Paying 350"),
+                  ],
                 ),
               )
+            ],
           );
         },
       ),
     );
   }
 }
+// child: Container(
+// width: 300,
+// height: 200,
+// padding: new EdgeInsets.all(10.0),
+// child: Card(
+// shape: RoundedRectangleBorder(
+// borderRadius: BorderRadius.circular(15.0),
+// ),
+// color: Colors.red,
+// elevation: 10,
+// child: Column(
+// mainAxisSize: MainAxisSize.min,
+// children: <Widget>[
+// const ListTile(
+// leading: Icon(Icons.album, size: 60),
+// title: Text(
+// 'Sonu Nigam',
+// style: TextStyle(fontSize: 30.0)
+// ),
+// subtitle: Text(
+// 'Best of Sonu Nigam Music.',
+// style: TextStyle(fontSize: 18.0)
+// ),
+// ),
+// ButtonBar(
+// children: <Widget>[
+// ElevatedButton(
+// child: const Text('Play'),
+// onPressed: () {/* ... */},
+// ),
+// ElevatedButton(
+// child: const Text('Pause'),
+// onPressed: () {/* ... */},
+// ),
+// ],
+// ),
+// ],
+// ),
+// ),
+// )
 
 class PlaceInfo {
   final String name;
